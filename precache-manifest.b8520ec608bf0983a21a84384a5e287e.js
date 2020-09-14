5cdc179f34a7bcbfd0fa487584052c7343919984
Your Name <you@example.com>
2020-09-14 23:11:39 +0900

    Updates

---
+++ precache-manifest.b8520ec608bf0983a21a84384a5e287e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774afde673ce1e4f64949bcbad2b4788",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "97a40ce9cbebf17b3e5d",
+    "url": "/movie_app_2020/static/css/main.059c54a6.chunk.css"
+  },
+  {
+    "revision": "c83928daac983263e9ea",
+    "url": "/movie_app_2020/static/js/2.637b91d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.637b91d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a40ce9cbebf17b3e5d",
+    "url": "/movie_app_2020/static/js/main.34319905.chunk.js"
+  },
+  {
+    "revision": "5cc71e1cb34edb9f47ad",
+    "url": "/movie_app_2020/static/js/runtime-main.1820d77f.js"
+  }
+]);
